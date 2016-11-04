a56ab25f147f0aad37a7b13208af0dd99e9f6761
Kevin Aud <kevinaud@gmail.com>
2016-11-04 15:54:06 -0500

    fixed alumni sign up

--- handler.js
+++ handler.js
@@ -3,10 +3,41 @@
 require('dotenv').config();
 
 var subscribeToRecruitment = require('./lib/subscribeToRecruitment');
+var subscribeToAlumni      = require('./lib/subscribeToAlumni');
 
 module.exports.subscribeToRecruitment = (event, context, callback) => {
 
   subscribeToRecruitment(JSON.parse(event.body)).then(function (result) {
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*" // Required for CORS support to work
+      },
+      body: JSON.stringify({ success: true, message: result }),
+    };
+
+    callback(null, response);
+
+  }, function (err) {
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*" // Required for CORS support to work
+      },
+      body: JSON.stringify({ success: false, message: err.message }),
+    };
+
+    callback(null, response);
+
+  });
+
+};
+
+module.exports.subscribeToAlumni = (event, context, callback) => {
+
+  subscribeToAlumni(JSON.parse(event.body)).then(function (result) {
 
     const response = {
       statusCode: 200,
